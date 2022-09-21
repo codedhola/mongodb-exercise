8f72f5aaf151cf994ebb8d1c7488b684f865a00a
codedhola <codedhola2000@gmail.com>
2022-09-21 14:27:25 +0100

    added find(): param id

--- index.js
+++ index.js
@@ -5,10 +5,16 @@ const app = express();
 app.use(express.json());
 
 app.get("/", async (req, res) => {
-    const data = await db.getDb().collection("clients").find({_id : ObjectId("6327a2a3f553fbe749aeb2fe")}).toArray()
+    const data = await db.getDb().collection("clients").find({}).toArray()
     console.log(data);
     res.send(data)
 }); 
+
+app.get("/:id", async (req, res) => {
+    const data = await db.getDb().collection("clients").find({ _id: ObjectId(req.params)}).toArray();
+    console.log(data);
+    res.send(data);
+});
 
 app.post("/post", async (req, res) => {
     const data = {
